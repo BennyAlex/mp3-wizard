79665d09efdd1eba9cd84f16a042d2af1e551e3c
Benjamin Franz <benjamin.franz@mainpost.de>
2017-06-27 16:46:05 +0200

    improve menu bar

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,15 @@
-import Ember from 'ember';
-const {Controller, inject} = Ember;
+import Ember from "ember"
+const {Controller, inject} = Ember
+const app = requireNode('electron').remote.app
 
 export default Controller.extend({
   title: inject.service(),
-  loading: inject.service()
+  loading: inject.service(),
+
+  actions: {
+    closeApp() {
+      if(this.get('loading.isLoading')) alert('There is a running process, please wait')
+      else app.quit()
+    }
+  }
 });
